17717558963bbdf6bd3b675ab2d373023c6f2c86
Joao Cabral de Matos Carvalho <jcmcgoojcmc@gmail.com>
2025-04-04 21:15:36 +0200

    Cambios en las rutas y variables

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,26 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
 
-Route::get('/', HomeController::class);
-Route::get('/posts', [PostController::class, 'index']);
-Route::get('/posts/create', [PostController::class, 'create']);
-Route::get('/posts/{post}', [PostController::class, 'show']);
+Route::get('/', function() {
+    return "Hola desde la página de inicio";
+
+    // return view('welcome');
+});
+Route::get('/contacto', function() {
+    return "Hola desde la página de contacto usando GET";
+});
+
+Route::get('/cursos/informacion', function() {
+    return "Aqui podrás encontrar toda la información de los cursos";
+});
+
+Route::get('/cursos/{curso}', function($curso) {
+ 
+    return "Bienvenido al curso: $curso";
+});
+
+
+
+
+
 

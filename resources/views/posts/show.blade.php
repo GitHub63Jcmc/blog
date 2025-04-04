04c15d5de5bd6e3ac5022f9fc084b02c44bc38f2
Joao Cabral de Matos Carvalho <jcmcgoojcmc@gmail.com>
2025-04-04 19:06:44 +0200

    first commit

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel 11/12 - SHOW</title>
+</head>
+<body>
+    <h1>Aqui se listarán todos los post {{ $post }} </h1>
+
+</body>
+</html>

0aaf851551e803364675ce3de58f1e456728fe29
Joao Cabral de Matos Carvalho <jcmcgoojcmc@gmail.com>
2025-04-12 21:27:46 +0200

    64 - Ejecusión de consultas de base de datos

--- routes/web.php
+++ routes/web.php
@@ -2,35 +2,23 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeController::class);
 
 // Route::prefix('posts')->name('posts.')->controller(PostController::class)->group(function() {
-//     // Ruta para mostrar el listado de registros
-//     Route::get('/', 'index')->name('index');
-    
-//     // Ruta para mostrar un formulario para crear un registro
-//     Route::get('/create')->name('create');
-    
-//     // Ruta para guardar un registro
-//     Route::post('/', 'store')->name('store');
-    
-//     // Ruta para mostrar un registro
-//     Route::get('/{post}', 'show')->name('show');
-    
-//     // Ruta para mostrar un formulario para editar un registro
-//     Route::get('/{post}/edit', 'edit')->name('edit');
-    
-//     // Ruta para actualizar un registros
-//     Route::put('/{post}', 'update')->name('update');
-    
-//     // Ruta para eliminar un registros
-//     Route::delete('/{post}', 'destroy')->name('destroy');
-// });
-
 
 Route::resource('posts', PostController::class);
 
 
+Route::get('/prueba', function(){
 
+    // Fitrar registros por ID
+    $categories = DB::table('categories')
+        ->where('id', '>', 1)
+        ->pluck('name', 'id');
+
+    return $categories;
+
+});
